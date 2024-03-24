fd1f9cb34d6d2f193f9d5dc640249e92441f11d5
satheeshML123 <satheeshwaran2405@outlook.com>
2024-03-25 01:20:56 +0530

    added workflow and test cases

---
+++ square.py
@@ -0,0 +1,3 @@
+
+def get_square(a):
+    return a*a
---
+++ test_square.py
@@ -0,0 +1,12 @@
+from square import get_square
+
+def test_get_square():
+    a = 4
+    results = get_square(a)
+    if results ==16:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(test_get_square())
